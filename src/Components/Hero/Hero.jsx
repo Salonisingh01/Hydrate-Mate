e3810c7e97932522f5961a7f424e4b945fdc3259
Saloni <saloniisingh93@gmail.com>
2025-03-29 13:39:35 +0530

    First Page Animation

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import BgImage from '../../assets/bg-slate.png';
 import BlackCoffee from '../../assets/black.png';
 import Navbar from '../Navbar/Navbar';
+import { motion } from "motion/react"
 
 const bgImage = {
     backgroundImage: `url(${BgImage})`,
@@ -14,46 +15,82 @@ const Hero = () => {
     return (
         <main style={bgImage}>
 
-            <section className='min-h-[750px] w-full'>
+            <section className='relative min-h-[750px] w-full'>
                 <div className="container">
-                    
+
                     {/* Navbar Section Here  */}
-                     <Navbar/>
+                    <Navbar />
 
                     {/* Hero Section Here  */}
                     <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 place-items-center min-h-[850px]' >
                         {/* text content Section  */}
                         <div className='text-[var(--color-lightOrange)] mt-[100px] md:mt-0 p-4 space-y-28'>
-                            <h1 className='text-7xl font-bold leading-tight ml-14' >Blvck Tumbler</h1>
-                            <div className='relative'>
+
+                            <motion.h1
+                                initial={{ opacity: 0, y: -100 }}
+                                animate={{ opacity: 1, y: 0 }}
+                                transition={{ type: 'spring', stiffness: 100, damping: 10, delay: 1 }}
+                                className='text-7xl font-bold leading-tight ml-14' >Blvck Tumbler
+                            </motion.h1>
+
+                            <motion.div
+                                initial={{ opacity: 0, y: 100 }}
+                                animate={{ opacity: 1, y: 0 }}
+                                transition={{ type: 'spring', stiffness: 100, damping: 10, delay: 1.2 }}
+
+                                className='relative' >
                                 <div className='relative z-10 space-y-4'>
                                     <h1 className='text-2xl'>Black Lifestyle Lovers</h1>
                                     <h1 className='text-sm opacity-55 leading-loose'> Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores,
                                         beatae dolores. Iste, quibusdam tempora molestias neque odit eos sunt
-                                         blanditiis placeat nihil
+                                        blanditiis placeat nihil
                                     </h1>
                                 </div>
                                 <div className='absolute -top-6 -left-10 w-[250px] h-[190px] bg-gray-700/25'></div>
-                            </div>
+                            </motion.div>
 
                         </div>
                         {/* Hero Image Section  */}
                         <div className='relative'>
-                            <img src={BlackCoffee} alt='' className='relative z-40 h-[400px] md:h-[700px] img-shadow' />
-                           
-                            {/* ring circke */}
-                            <div className=' h-[180px] w-[180px] absolute top-24 -right-16 border-[var(--color-primary)] border-[20px] rounded-full z-10'></div>
-                            
-                             {/* Big text content section */}
-                            <div className='absolute -top-10 left-[350px] z-[1]'>
+                            <motion.img  
+                             initial={{ opacity: 0, scale: 0 }} 
+                             animate={{ opacity: 1, scale: 1 }}
+                             transition={{ 
+                                    type: 'spring', 
+                                    stiffness: 100, 
+                                    damping: 10, delay: 0.4 
+                                }} 
+                              src={BlackCoffee} alt='' className='relative z-40 h-[400px] md:h-[700px] img-shadow' />
+
+                            {/* ring circle */}
+                            <motion.div 
+                               initial={{ opacity: 0, y: 100 }}
+                               animate={{ opacity: 1, y: 0 }}
+                               transition={{ type: 'spring', stiffness: 100, damping: 10, delay: 0.8 }}
+                               
+                            className=' h-[180px] w-[180px] absolute top-24 -right-16 border-[var(--color-primary)] border-[20px] rounded-full z-10'></motion.div>
+
+                            {/* Big text  section */}
+                            <motion.div 
+                             initial={{ opacity: 0, x: -100 }}
+                             animate={{ opacity: 1, x: 0 }}
+                             transition={{ type: 'spring', stiffness: 100, damping: 10, delay: 0.8 }}
+                              
+                             className='absolute -top-10 left-[350px] z-[1]'>
                                 <h1 className='text-[120px] scale-150 font-bold text-[var(--color-darkGary)]/40 leading-none]'>
-                                  Blvck Tumbler
+                                    Blvck Tumbler
                                 </h1>
 
-                            </div>
+                            </motion.div>
                         </div>
                         {/* third div section */}
-                        <div className='text-[var(--color-lightOrange)] mt-[100px] md:mt-0 p-4 space-y-28'>
+                        <motion.div 
+                         initial={{ opacity: 0, y: 100 }}
+                         animate={{ opacity: 1, y: 0 }}
+                         transition={{ type: 'spring', stiffness: 100, damping: 10, delay: 1.2 }}
+                         
+                         className='text-[var(--color-lightOrange)] mt-[100px] md:mt-0 p-4 space-y-28'>
+
                             <h1 className='opacity-0 text-7xl font-bold leading-tight ml-14' >Hydrate Mate</h1>
                             <div className='relative'>
                                 <div className='relative z-10 space-y-4'>
@@ -68,11 +105,14 @@ const Hero = () => {
                                 <div className='absolute -top-6 -right-10 w-[250px] h-[190px] bg-[var(--color-darkGary)]/50'></div>
                             </div>
 
-                        </div>
-                        
+                        </motion.div>
+
                         <div></div>
                     </div>
                 </div>
+                
+  {/* sidebar bar menu Section  */}
+
             </section>
 
         </main>
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,20 +1,32 @@
 import React from 'react'
 import { GiHamburgerMenu } from "react-icons/gi";
+import { motion } from 'motion/react'; 
 
 const Navbar = () => {
   return (
-    <nav className='absolute top-0 left-12 w-[1430px] pt-10 text-white z-20'>
+    <nav className='absolute top-0 left-12 w-[1430px] pt-10 text-white z-40'>
         <div className="container">
             <div className='flex  justify-between items-center'>
            {/* logo section */}
-           <h1 className='text-2xl font-semibold uppercase '> 
+           <motion.h1 
+
+            initial={{ opacity: 0, y: -100 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ type: 'spring', stiffness: 100, damping: 10, delay: 0.2 }}
+
+           className='text-2xl font-semibold uppercase '> 
              <span  className='text-[var(--color-primary)] '>Hydrate</span> Mate
-           </h1>
+           </motion.h1>
 
            {/* Hamburger Menu section */}
-           <div>
+           <motion.div 
+            initial={{ opacity: 0, y: -100 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ type: 'spring', stiffness: 100, damping: 10, delay: 0.2 }}
+
+           >
            <GiHamburgerMenu className='text-3xl cursor-pointer'/>
-           </div>
+           </motion.div>
 
             </div>
         </div>
